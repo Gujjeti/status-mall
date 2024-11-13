fb72714782301f6a36a1a3e23e1574168970b58e
Ganesh Gujjeti (Annalect) <ganesh.gujjeti@annalect.com>
2024-11-13 11:40:36 +0530

    first commit

--- js/main.js
+++ js/main.js
@@ -61,8 +61,8 @@ tl1.addLabel('start')
 });
 
 tl4.addLabel('start')
-    .from('.box-icon span, .box-icon span svg', { scale: 0.3, autoAlpha: 0 })
-    .from('.box-icon p', { y:50, stagger:0.1, autoAlpha: 0 })
+    .from('.section-4 .box-icon span, .section-4 .box-icon span svg', { scale: 0.3, autoAlpha: 0 })
+    .from('.section-4 .box-icon p', { y:50, stagger:0.1, autoAlpha: 0 })
 
 
 
@@ -80,26 +80,113 @@ tl4.addLabel('start')
     .from('.section-5 .envelope', { x:-200, autoAlpha: 0 })
 
 
-    document.querySelector('.close-env').addEventListener('click', function() {
-      console.log('as')
-      this.classList.toggle('opened');
+
+    let tl6 = gsap.timeline({
+      // yes, we can add it to an entire timeline!
+      scrollTrigger: {
+          trigger: '.section-6',
+          pin: true, // pin the trigger element while active
+          start: 'top top', // when the top of the trigger hits the top of the viewport
+          end: '+=500', // end after scrolling 500px beyond the start
+          scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+        
+      }
+  });
+  
+  // add animations and labels to the timeline
+  tl6.addLabel('start')
+      .from('.section-6 .heading-1', { y:window.innerHeight, autoAlpha: 0 })
+ 
+
+
+
+          let tl8 = gsap.timeline({
+      // yes, we can add it to an entire timeline!
+      scrollTrigger: {
+          trigger: '.section-8',
+          pin: true, // pin the trigger element while active
+          start: 'top top', // when the top of the trigger hits the top of the viewport
+          end: '+=500', // end after scrolling 500px beyond the start
+          scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+      }
+  });
+  
+  // add animations and labels to the timeline
+  tl8.addLabel('start')
+      .from('.section-8 .heading-1', { y:window.innerHeight, autoAlpha: 0 })
+    
+
+
+      let tl9 = gsap.timeline({
+        // yes, we can add it to an entire timeline!
+        scrollTrigger: {
+            trigger: '.section-9',
+            start: 'top 40%',
+           
+        
+        }
     });
+    
+    tl9.addLabel('start')
+        .from('.section-9 .box-icon span, .section-9 .box-icon span svg', { scale: 0.3, autoAlpha: 0 })
+        .from('.section-9 .box-icon p', { y:50, stagger:0.1, autoAlpha: 0 })
+
+      
+      let tl10 = gsap.timeline({
+        scrollTrigger: {
+            trigger: '.section-10',
+            start: 'top top',
+            end: '+=700',
+            pin: true,
+            scrub: 1,
+        }
+    });
+    
+    tl10.addLabel('start')
+    .from(['.section-10 .heading-1, .section-10 small, .section-10 p'], { y:100, stagger:0.2, autoAlpha: 0 })
+    .from(['.section-10 img'], { scale:0,  autoAlpha: 0 })
+
+      document.querySelector('.envelope').addEventListener('click', function() {
+        this.classList.toggle('opened');
+    });
+    
+    // Prevent the envelope from closing when clicking on the form
+    document.querySelector('.letter').addEventListener('click', function(event) {
+        event.stopPropagation();
+    });
+    
+    document.querySelector('.letter').addEventListener('submit', function(event) {
+        event.preventDefault();
+        this.reset();
+        document.querySelector('.envelope').classList.remove('opened'); // Optional: close envelope after submission
+    });
+    
+    
+  
+    
+
+    function showDiv(id, element) {
+      // Remove 'show' class from all elements with 'floorDetails' class
+      document.querySelectorAll('.floorAvailList').forEach(div => {
+        div.classList.remove('show');
+      });
+      
+      // Add 'show' class to the specific div with the provided id
+      const targetDiv = document.getElementById(id);
+      if (targetDiv) {
+        targetDiv.classList.add('show');
+      }
+
+   document.querySelectorAll('#floors li').forEach(li => {
+    li.classList.remove('active');
+  });
+  
+  // Add 'active' class to the clicked <li> element
+  element.classList.add('active');
 
 
-  document.querySelector('.envelope').addEventListener('click', function() {
-    this.classList.toggle('opened');
-});
-
-// Prevent the envelope from closing when clicking on the form
-document.querySelector('.letter').addEventListener('click', function(event) {
-    event.stopPropagation();
-});
-
-document.querySelector('.letter').addEventListener('submit', function(event) {
-    event.preventDefault();
-    this.reset();
-    document.querySelector('.envelope').classList.remove('opened'); // Optional: close envelope after submission
-});
+    }
+    
 
 
 
